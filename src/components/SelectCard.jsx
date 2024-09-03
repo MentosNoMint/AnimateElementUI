4cf116d768523d3e88b9e15670d48de2c807a68e
MentosNoMint <dima-krut-2020@bk.ru>
2024-09-04 02:26:59 +0500

    update adaptive

--- src/components/SelectCard.jsx
+++ src/components/SelectCard.jsx
@@ -3,11 +3,11 @@ import '../App.css'
 function SelectCard() {
     return (
         <div className='flex flex-col'>
-            <div className='flex gap-[20px]'>
+            <div className='flex gap-[20px] flex-wrap justify-center'>
                 <div className='bg-[#E8F1CD] w-[373px] h-[170px] rounded-[18px] flex cursor-pointer'>
                     <div className='flex flex-col h-full min-w-[203px]'>
                         <h1 className='font-semibold text-[20px] ml-[25px] mt-[20px]'>Разработчик игр</h1>
-                        <p className='opacity-[0.8] font-medium ml-[25px] max-w-[152px] text-[14px]'>Научитесь создавать игры и 3D модели</p>
+                        <p className='opacity-[0.8] font-medium ml-[25px] max-w-[152px] text-[13px]'>Научитесь создавать игры и 3D модели</p>
                         <div className='flex ml-[25px] h-full items-end'>
                             <div className='bg-[#FFE178] rounded-[46px] flex justify-center items-center mb-[14px]'>
                                 <span className='text-[14px] px-[8px] py-[4px] font-medium'>11 классов</span>
@@ -24,7 +24,7 @@ function SelectCard() {
                 <div className='bg-[#CDEBF1] w-[373px] h-[170px] rounded-[18px] flex cursor-pointer'>
                     <div className='flex flex-col h-full min-w-[203px]'>
                         <h1 className='font-semibold text-[20px] ml-[25px] mt-[20px]'>Разработчик веб</h1>
-                        <p className='opacity-[0.8] font-medium ml-[25px] max-w-[152px] text-[14px]'>Научитесь создавать веб-сайты</p>
+                        <p className='opacity-[0.8] font-medium ml-[25px] max-w-[152px] text-[13px]'>Научитесь создавать веб-сайты</p>
                         <div className='flex ml-[25px] h-full items-end'>
                             <div className='bg-[#FFE178] rounded-[46px] flex justify-center items-center mb-[14px]'>
                                 <span className='text-[14px] px-[8px] py-[4px] font-medium'>11 классов</span>
@@ -39,11 +39,11 @@ function SelectCard() {
                     </div>
                 </div>
             </div>
-            <div className='flex gap-[20px] mt-[20px]'>
+            <div className='flex gap-[20px] mt-[20px] flex-wrap justify-center'>
                 <div className='bg-[#F1EDCD] w-[373px] h-[170px] rounded-[18px] flex cursor-pointer'>
                     <div className='flex flex-col h-full min-w-[203px]'>
                         <h1 className='font-semibold text-[20px] ml-[25px] mt-[20px]'>Разработчик 1C</h1>
-                        <p className='opacity-[0.8] font-medium ml-[25px] max-w-[152px] text-[14px]'>Научитесь работать с таблицами в 1C</p>
+                        <p className='opacity-[0.8] font-medium ml-[25px] max-w-[152px] text-[13px]'>Научитесь работать с таблицами в 1C</p>
                         <div className='flex ml-[25px] h-full items-end'>
                             <div className='bg-[#FFE178] rounded-[46px] flex justify-center items-center mb-[14px]'>
                                 <span className='text-[14px] px-[8px] py-[4px] font-medium'>11 классов</span>
@@ -60,7 +60,7 @@ function SelectCard() {
                 <div className='bg-[#CDD1F1] w-[373px] h-[170px] rounded-[18px] flex cursor-pointer'>
                     <div className='flex flex-col h-full min-w-[203px]'>
                         <h1 className='font-semibold text-[20px] ml-[25px] mt-[20px]'>Разработчик икс</h1>
-                        <p className='opacity-[0.8] font-medium ml-[25px] max-w-[152px] text-[14px]'>Научитесь работать с программой икс</p>
+                        <p className='opacity-[0.8] font-medium ml-[25px] max-w-[152px] text-[13px]'>Научитесь работать с программой икс</p>
                         <div className='flex ml-[25px] h-full items-end'>
                             <div className='bg-[#FFE178] rounded-[46px] flex justify-center items-center mb-[14px]'>
                                 <span className='text-[14px] px-[8px] py-[4px] font-medium'>11 классов</span>
