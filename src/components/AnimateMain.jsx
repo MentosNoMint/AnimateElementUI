6f587307cdec59ccebae4b417250f6e3d9b47219
Daria <d.shkarban01@gmail.com>
2024-09-03 16:17:14 +0500

    first element

---
+++ src/components/AnimateMain.jsx
@@ -0,0 +1,12 @@
+import '../App.css';
+import SelectCard from './SelectCard';
+
+function AnimateMain() {
+    return (
+        <div className='wrapper'>
+            <SelectCard />
+        </div>
+    );
+}
+
+export default AnimateMain;
---
+++ src/components/SelectCard.jsx
@@ -0,0 +1,82 @@
+import '../App.css'
+
+function SelectCard() {
+    return (
+        <div className='flex flex-col'>
+            <div className='flex gap-[20px]'>
+                <div className='bg-[#E8F1CD] w-[373px] h-[170px] rounded-[18px] flex cursor-pointer'>
+                    <div className='flex flex-col h-full min-w-[203px]'>
+                        <h1 className='font-semibold text-[20px] ml-[25px] mt-[20px]'>Разработчик игр</h1>
+                        <p className='opacity-[0.8] font-medium ml-[25px] max-w-[152px] text-[14px]'>Научитесь создавать игры и 3D модели</p>
+                        <div className='flex ml-[25px] h-full items-end'>
+                            <div className='bg-[#FFE178] rounded-[46px] flex justify-center items-center mb-[14px]'>
+                                <span className='text-[14px] px-[8px] py-[4px] font-medium'>11 классов</span>
+                            </div>
+                            <div className='bg-[#FFE178] rounded-[46px] flex justify-center items-center ml-[5px] mb-[14px]'>
+                                <span className='text-[14px] px-[8px] py-[4px] font-medium'>9 классов</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex w-full items-end justify-end relative'>
+                        <img src="img/gamepad.svg" alt="gamepad" className='mb-[-16px] mr-[-20px] absolute' />
+                    </div>
+                </div>
+                <div className='bg-[#CDEBF1] w-[373px] h-[170px] rounded-[18px] flex cursor-pointer'>
+                    <div className='flex flex-col h-full min-w-[203px]'>
+                        <h1 className='font-semibold text-[20px] ml-[25px] mt-[20px]'>Разработчик веб</h1>
+                        <p className='opacity-[0.8] font-medium ml-[25px] max-w-[152px] text-[14px]'>Научитесь создавать веб-сайты</p>
+                        <div className='flex ml-[25px] h-full items-end'>
+                            <div className='bg-[#FFE178] rounded-[46px] flex justify-center items-center mb-[14px]'>
+                                <span className='text-[14px] px-[8px] py-[4px] font-medium'>11 классов</span>
+                            </div>
+                            <div className='bg-[#FFE178] rounded-[46px] flex justify-center items-center ml-[5px] mb-[14px]'>
+                                <span className='text-[14px] px-[8px] py-[4px] font-medium'>9 классов</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex w-full items-end justify-end relative'>
+                        <img src="img/labtop.svg" alt="gamepad" className='mb-[-16px] mr-[-14px] absolute' />
+                    </div>
+                </div>
+            </div>
+            <div className='flex gap-[20px] mt-[20px]'>
+                <div className='bg-[#F1EDCD] w-[373px] h-[170px] rounded-[18px] flex cursor-pointer'>
+                    <div className='flex flex-col h-full min-w-[203px]'>
+                        <h1 className='font-semibold text-[20px] ml-[25px] mt-[20px]'>Разработчик 1C</h1>
+                        <p className='opacity-[0.8] font-medium ml-[25px] max-w-[152px] text-[14px]'>Научитесь работать с таблицами в 1C</p>
+                        <div className='flex ml-[25px] h-full items-end'>
+                            <div className='bg-[#FFE178] rounded-[46px] flex justify-center items-center mb-[14px]'>
+                                <span className='text-[14px] px-[8px] py-[4px] font-medium'>11 классов</span>
+                            </div>
+                            <div className='bg-[#FFE178] rounded-[46px] flex justify-center items-center ml-[5px] mb-[14px]'>
+                                <span className='text-[14px] px-[8px] py-[4px] font-medium'>9 классов</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex w-full items-end justify-end relative'>
+                        <img src="img/table.svg" alt="gamepad" className='mb-[-16px] mr-[-14px] absolute' />
+                    </div>
+                </div>
+                <div className='bg-[#CDD1F1] w-[373px] h-[170px] rounded-[18px] flex cursor-pointer'>
+                    <div className='flex flex-col h-full min-w-[203px]'>
+                        <h1 className='font-semibold text-[20px] ml-[25px] mt-[20px]'>Разработчик икс</h1>
+                        <p className='opacity-[0.8] font-medium ml-[25px] max-w-[152px] text-[14px]'>Научитесь работать с программой икс</p>
+                        <div className='flex ml-[25px] h-full items-end'>
+                            <div className='bg-[#FFE178] rounded-[46px] flex justify-center items-center mb-[14px]'>
+                                <span className='text-[14px] px-[8px] py-[4px] font-medium'>11 классов</span>
+                            </div>
+                            <div className='bg-[#FFE178] rounded-[46px] flex justify-center items-center ml-[5px] mb-[14px]'>
+                                <span className='text-[14px] px-[8px] py-[4px] font-medium'>9 классов</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex w-full items-end justify-end relative'>
+                        <img src="img/book.svg" alt="gamepad" className='mb-[-16px] mr-[-14px] absolute' />
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default SelectCard
